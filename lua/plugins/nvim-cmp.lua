d3301ff8383e938d3cf2b5d3a2a3b92cbd063cca
Konstantin Tutsch <mail@konstantintutsch.com>
2025-08-11 20:37:07 +0200

    fix: configure dependencies of nvim-cmp as dependencies
    Signed-off-by: Konstantin Tutsch <mail@konstantintutsch.com>

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,3 +1,10 @@
 return {
-    "hrsh7th/nvim-cmp"
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "hrsh7th/vim-vsnip", dependencies = {
+                { "hrsh7th/cmp-vsnip" }
+            }
+        }
+    }
 }
