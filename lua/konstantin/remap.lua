82613d2906f9ce1407fc027ed35752d8ef0a94de
Konstantin Tutsch <mail@konstantintutsch.com>
2025-08-11 20:50:09 +0200

    feat: add keymap to go to definition
    Signed-off-by: Konstantin Tutsch <mail@konstantintutsch.com>

--- lua/konstantin/remap.lua
+++ lua/konstantin/remap.lua
@@ -12,4 +12,5 @@ end)
 -- LSP
 vim.keymap.set("n", "<leader>i", vim.lsp.buf.hover)
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
+vim.keymap.set("n", "<leader>gt", vim.lsp.buf.definition)
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
