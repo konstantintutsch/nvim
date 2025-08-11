4dc33f43c3897d2c258fe1918a81c7502e015b13
Konstantin Tutsch <mail@konstantintutsch.com>
2025-08-11 19:35:11 +0200

    feat: add keymap to view diagnostics
    Signed-off-by: Konstantin Tutsch <mail@konstantintutsch.com>

--- lua/konstantin/remap.lua
+++ lua/konstantin/remap.lua
@@ -11,4 +11,5 @@ end)
 
 -- LSP
 vim.keymap.set("n", "<leader>i", vim.lsp.buf.hover)
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
