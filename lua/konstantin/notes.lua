ec13fb8d9179d8510650a24b17b234a1be593c11
Konstantin Tutsch <mail@konstantintutsch.com>
2025-08-18 16:11:55 +0200

    feat: add note creation
    Signed-off-by: Konstantin Tutsch <mail@konstantintutsch.com>

---
+++ lua/konstantin/notes.lua
@@ -0,0 +1,25 @@
+note = {}
+note.directory = "~/Documents/Notizen"
+
+function note.new()
+    local title = vim.fn.input("Titel: ")
+    if title == "" then
+        return
+    end
+
+    local file = note.directory .. "/" .. title .. ".md"
+
+    vim.cmd("edit " .. file)
+
+    -- only add title to new notes
+    if not vim.loop.fs_stat(file) then
+        vim.api.nvim_buf_set_lines(0, 0, -1, false, {
+          "# " .. title,
+          "",
+        })
+    end
+
+    vim.cmd("normal! G")
+end
+
+vim.keymap.set("n", "<C-n>", note.new)
