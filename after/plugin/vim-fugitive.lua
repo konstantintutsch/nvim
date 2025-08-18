e540817c1e63df4c689a5e6862e9fa1834d82129
Konstantin Tutsch <mail@konstantintutsch.com>
2025-08-18 16:15:16 +0200

    feat: add remaps for committing, pulling and pushing
    Signed-off-by: Konstantin Tutsch <mail@konstantintutsch.com>

--- after/plugin/vim-fugitive.lua
+++ after/plugin/vim-fugitive.lua
@@ -1,1 +1,4 @@
 vim.keymap.set("n", "<leader>g", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gc", function () vim.cmd("G commit --signoff") end)
+vim.keymap.set("n", "<leader>gd", function () vim.cmd("G pull") end)
+vim.keymap.set("n", "<leader>gp", function () vim.cmd("G push") end)
